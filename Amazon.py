f83c4a299414ea7f0cf6ad3c9938a37d8811f574
Debasis Baidya <speak2debasis@gmail.com>
2025-04-27 17:43:19 +0530

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -121,7 +121,7 @@ if "input_key" not in st.session_state:
 # Header section with styling
 st.markdown("""
 <div style='text-align: center; padding: 15px; border: 1px solid #ddd; border-radius: 10px;'>
-    <h1>💬 Real-time Sentiment Classifier</h1>
+    <h1>📦 Amazon Reviews Sentiment Analyzer 🛒💳</h1>
     <p style='font-size:16px;'>Classify product reviews as <b style='color:green;'>Positive</b>, <b style='color:orange;'>Neutral</b>, or <b style='color:red;'>Negative</b></p>
 </div>
 """, unsafe_allow_html=True)
