38085042f60f8c10e17d96031ce0cc3f10719872
Debasis Baidya <speak2debasis@gmail.com>
2025-04-27 18:53:34 +0530

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -375,9 +375,7 @@ if st.session_state["prediction_result"] is not None:
     # Footer note about the model
     st.markdown("""
     <div style='text-align:center; padding-top: 10px;'>
-        <span style='font-size:13px; color: gray;'>🤖 Powered by Neural Network | TF-IDF + Engineered Features</span>
-    </div>
-    """, unsafe_allow_html=True)
+        <span style='font-size:13px;
 
     # Celebrate prediction with balloons animation
     st.balloons()
